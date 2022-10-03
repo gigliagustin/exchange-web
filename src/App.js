388d68b04b4b32bbdfb83c7bdc1ef42621dc3a15
unknown <dante.sarmientof5@gmail.com>
2022-10-03 12:03:29 -0300

    test file 'Header' deleted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Header from './components/Header';
-import {
-  ThemeProvider,
-} from './providers/ThemeProvider';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <div className='App'>
-        <Header />
-      </div>
-    </ThemeProvider>
+    <div>
+    </div>
   );
 };
 
