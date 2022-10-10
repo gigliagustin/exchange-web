529c1f85122dacb3a7f77c4b2bf964e4e20a6301
Maciel Castro <mlcastro@todo1.net>
2022-10-10 15:52:46 -0300

    Add BlankSlate test

--- src/components/BlankSlate/BlankSlate.js
+++ src/components/BlankSlate/BlankSlate.js
@@ -7,7 +7,7 @@ const BlankSlate = () => {
       <img
         src={cryptos}
         alt='Cryptos image'
-        className='w-75'
+        className='w-25'
       />
       <h2 className='text-center my-0'>Please, to continue choose a currency...</h2>
     </div>
