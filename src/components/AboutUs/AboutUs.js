a57c9003ce8ec8a359bd41c3c3129a1864febbdb
juancansi <juancansi@gmail.comgit config --global user.name Your>
2022-10-10 21:37:53 -0300

    Add styles and text

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -5,10 +5,10 @@ import './aboutUs.scss';
 const AboutUs = () => {
   return (
     <div className='container-fluid' id='about-us'>
-      <p className='p-1 m-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p className='p-2  mt-4 mb-4 fs-4 text-center'>Somos alumnos de la comisión de React Advanced en Rolling Code. Estamos presentando nuestro primer proyecto implementando todos los conocimientos adquiridos hasta el momento.</p>
       <div className="container-fluid d-flex justify-content-around flex-wrap ">
         {TeamMembers.map((members) => <div className='' key={members.id}>
-          <img src={ members.image } alt="" className='memberImage m-2' />
+          <img src={members.image} alt="" className='memberImage m-2' />
           <p className='text-center fw-bold m-0' >{members.name}</p>
           <p className='text-center fw-light' >{members.role}</p>
         </div>)}
